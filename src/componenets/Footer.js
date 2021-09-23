ce80fab7ff65699a4133e6d8b71c0ca3f21b1a16
zakaria benkali <benkalizakaria@gmail.com>
2021-09-23 19:53:48 +0100

    update

---
+++ src/componenets/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>Benzaki Web Development Copyright © 2021</p>
+        </footer>
+    )
+}
+
+export default Footer
--- src/componenets/Weather.js
+++ src/componenets/Weather.js
@@ -4,6 +4,7 @@ import { FaSearch } from 'react-icons/fa';
 import CurrentWeather from './CurrentWeather';
 import HourlyWeather from "./HourlyWeather";
 import { FaCircleNotch } from 'react-icons/fa';
+import Footer from "./Footer";
 require('dotenv').config();
 
 const Weather = ({ geolocation }) => {
@@ -11,8 +12,7 @@ const Weather = ({ geolocation }) => {
     const [query, setQuery] = useState('')
     const [searchDebounce, setSearchDebounce] = useState('');
     const [unit, setUnit] = useState('metric')
-    // const [active, setActive] = useState(false)
-    // const [open, setOpen] = useState(false)
+
     let defaultCity = geolocation.district;
 
     useEffect(() => {
@@ -34,31 +34,38 @@ const Weather = ({ geolocation }) => {
     const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
     const CURRENT_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${API_KEY}`
     const HOURLY_WEATHER_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${unit}&cnt=6&appid=${API_KEY}`
-    
+
     // Get weather data
-    const { data: currentWeather, isLoading} = useFetch(CURRENT_WEATHER_URL)
+    const { data: currentWeather, isLoading } = useFetch(CURRENT_WEATHER_URL)
     console.log(currentWeather);
 
     const { data: hourlyWeather } = useFetch(HOURLY_WEATHER_URL)
     console.log(hourlyWeather);
 
+
     return (
         <div>
-            {isLoading && <FaCircleNotch className='spinner'/> }
+            {isLoading && <FaCircleNotch className='spinner' />}
             {currentWeather &&
                 <div className="weather-container">
                     <form className="form" onSubmit={handleSearch} >
                         <input type="text" placeholder="Search for location" value={query} onChange={e => setQuery(e.target.value)} />
                         <FaSearch className='search-icon' onClick={handleSearch} />
                     </form>
-                    <button className="btn" onClick={() => setUnit('metric')}>°C</button>
-                    <button className="btn" onClick={() => setUnit('imperial')}>°F</button>
+                    <button className="btn"
+                        onClick={() => setUnit('metric')}
+                    >°C</button>
+
+                    <button className="btn"
+                        onClick={() => setUnit('imperial')}
+                    >°F</button>
                     <CurrentWeather weather={currentWeather} unit={unit} />
                     <HourlyWeather hourlyWeather={hourlyWeather} />
+                    <Footer />
                 </div>
             }
         </div>
     )
-}
 
+}
 export default Weather
